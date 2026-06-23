3a0d875631bdcb6b115e8d856d0aa9c4779f40ca
Alencar Prior <aleprior@outlook.com>
2026-06-23 20:30:15 -0300

    Animações e exclusão de botões invisíveis.
    Os botões invisíveis para as trocas de tela pré-menu foram removidos. Agora as funções de troca de telas são ativadas diretamente ao clicar nos textos, cujos quais receberam melhorias visuais de brilho e sublinhagem ao passar o mouse por cima. No menu, fora adicionado uma animação que exalta apenas o botão selecionado pelo usuário, diminuindo o tamanho dos outros.

--- src/sistema/view/controladores/Controller.java
+++ src/sistema/view/controladores/Controller.java
@@ -1,12 +1,13 @@
 package sistema.view.controladores;
 
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.Glow;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.Modality;
@@ -15,14 +16,43 @@ import java.io.IOException;
 
 public class Controller {
 
-    @FXML private Text retornarLogin;
-    
+    @FXML private Text textoMudanca;
+    @FXML private Text textoMudanca2;
+
+
     private Stage stage;
     private Scene scene;
     private Parent root;
     //@FXML private Button btnCodigo;
 
-    public void mudarParaLogin(ActionEvent event) throws IOException {
+    @FXML public void initialize() {
+
+
+        Glow brilho = new Glow(0.4);
+        Glow brilhodesativado = new Glow(0.0);
+
+        if (textoMudanca != null) {
+            textoMudanca.setOnMouseEntered(e -> {
+                textoMudanca.setStyle("-fx-underline: true");
+                textoMudanca.setEffect(brilho);
+            });
+            textoMudanca.setOnMouseExited(e -> {
+                textoMudanca.setStyle("-fx-underline: false");
+                textoMudanca.setEffect(brilhodesativado);
+            });
+        }
+        if (textoMudanca2 != null) {
+            textoMudanca2.setOnMouseEntered(e -> {
+                textoMudanca2.setStyle("-fx-underline: true");
+                textoMudanca2.setEffect(brilho);
+            });
+            textoMudanca2.setOnMouseExited(e -> {
+                textoMudanca2.setStyle("-fx-underline: false");
+                textoMudanca2.setEffect(brilhodesativado);
+            });
+        }
+    }
+    public void mudarParaLogin(Event event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("erpLogin.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -31,7 +61,7 @@ public class Controller {
         stage.show();
     }
 
-    public void mudarParaCadastro(ActionEvent event) throws IOException {
+    public void mudarParaCadastro(Event event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("erpCadastroUsuario.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -40,7 +70,7 @@ public class Controller {
         stage.show();
     }
 
-    public void mudarparaRecuperacao(ActionEvent event) throws IOException {
+    public void mudarparaRecuperacao(Event event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("erpRecuperacao.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -49,7 +79,7 @@ public class Controller {
         stage.show();
     }
 
-    public void gerarCodigoRecuperacao(ActionEvent event) throws IOException {
+    public void gerarCodigoRecuperacao(Event event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("erpCodigo.fxml"));
         Stage popCodigo = new Stage();
         popCodigo.setTitle("");
@@ -62,7 +92,7 @@ public class Controller {
 
 
 
-    public void mudarParaMenu(ActionEvent event) throws IOException {
+    public void mudarParaMenu(Event event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("erpMenu.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
