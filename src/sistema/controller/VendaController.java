0cdd99ef4e3274e172c673cf6fe8ab1aadb829b9
Alencar Prior <aleprior@outlook.com>
2026-06-28 19:10:45 -0300

    Integração da lista nas configurações e ato de exclusão de registros.

---
+++ src/sistema/controller/VendaController.java
@@ -0,0 +1,36 @@
+package sistema.controller;
+
+import sistema.model.Usuario;
+import sistema.model.Venda;
+import sistema.service.VendaService;
+
+import java.util.List;
+
+public class VendaController {
+
+    private final VendaService vendaService = new VendaService();
+
+    public int registrarVenda(
+            String idCliente,
+            String idProduto,
+            String quantidade,
+            String valorUnitario,
+            String desconto,
+            String idFormaPagamento,
+            Usuario usuarioLogado
+    ) {
+        return vendaService.registrarVenda(
+                idCliente,
+                idProduto,
+                quantidade,
+                valorUnitario,
+                desconto,
+                idFormaPagamento,
+                usuarioLogado
+        );
+    }
+
+    public List<Venda> listarVendas() {return vendaService.listarVendas();}
+
+    public void excluirVenda(String idTexto){vendaService.excluirVenda(idTexto);}
+}
